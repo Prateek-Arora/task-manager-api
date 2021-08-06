e2e16d311c7943622361c6900296222516c38ab1
Prateek Arora <official.aroraprateek@gmail.com>
2021-08-06 22:37:05 +0530

    Initial Commit

---
+++ src/emails/account.js
@@ -0,0 +1,40 @@
+const sgMail = require("@sendgrid/mail");
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const sendWelcomeEmail = (email, name) => {
+  sgMail
+    .send({
+      to: email,
+      from: "official.aroraprateek@gmail.com",
+      subject: "Welcome to Task Manager App.",
+      text: `Welcome to the app, ${name}. Let me know how you get along with the app.`,
+    })
+    .then(() => {
+      console.log("Email Sent.");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+const sendCancelationEmail = (email, name) => {
+  sgMail
+    .send({
+      to: email,
+      from: "official.aroraprateek@gmail.com",
+      subject: "Sorry to see you go.",
+      text: `GoodBye, ${name}. I hope to see you sometime soon again.`,
+    })
+    .then(() => {
+      console.log("Email Sent.");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+module.exports = {
+  sendWelcomeEmail,
+  sendCancelationEmail,
+};
