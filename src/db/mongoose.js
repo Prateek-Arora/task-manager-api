f7d9a73c623aec1a2f3e18377c947e93c625f457
Prateek Arora <official.aroraprateek@gmail.com>
2021-08-06 23:40:42 +0530

    changed environment variables

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 
-mongoose.connect(process.env.MONGODB_URL, {
+mongoose.connect(process.env.MONGODBURL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
--- src/emails/account.js
+++ src/emails/account.js
@@ -1,6 +1,6 @@
 const sgMail = require("@sendgrid/mail");
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+sgMail.setApiKey(process.env.SENDGRIDAPIKEY);
 
 const sendWelcomeEmail = (email, name) => {
   sgMail
